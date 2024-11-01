11d91ab89b9ba717e1f0f795e67511bfad965abf
Frank Mugendi <njaombefrank@gmail.com>
2024-11-01 12:11:52 +0300

    react checkpoint

---
+++ my-app/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const myComponent = ({ title }) => <h1>{title} </h1>;
+
+export default myComponent;
+<myComponent title="Im Learning React" />;
+console.log(myComponent);
